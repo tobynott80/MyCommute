2ae0270717a9c090bb67b9dad9e11fbb5554149c
notttj <tobynott80@gmail.com>
2023-02-05 18:23:07 +0000

    init android project

---
+++ 4-application/app/src/main/java/uk/ac/cardiff/c21048228/mycommute/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package uk.ac.cardiff.c21048228.mycommute.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
