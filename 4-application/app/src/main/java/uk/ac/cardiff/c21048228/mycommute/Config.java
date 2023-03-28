1aaa3d05cd257b266ab71a7c3b99faec222ed4c6
notttj <tobynott80@gmail.com>
2023-03-28 16:58:58 +0100

    added api creds and config

---
+++ 4-application/app/src/main/java/uk/ac/cardiff/c21048228/mycommute/Config.java
@@ -0,0 +1,8 @@
+package uk.ac.cardiff.c21048228.mycommute;
+
+public class Config {
+    public static String PACKAGE_NAME = "uk.ac.cardiff.c21048228.mycommute";
+    public static final String API_BASE_URL = "http://api.rtt.io/api/v1/json/";
+    public final static String API_KEY = "XX-API-KEY-XX==";
+
+}
