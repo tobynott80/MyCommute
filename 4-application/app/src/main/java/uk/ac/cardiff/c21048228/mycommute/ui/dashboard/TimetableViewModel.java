c7992fba43548c6e657438de3dc6499bcd98ca0f
notttj <tobynott80@gmail.com>
2023-04-01 00:06:30 +0100

    fixed manifest and some string hardcoding errors

--- 4-application/app/src/main/java/uk/ac/cardiff/c21048228/mycommute/ui/dashboard/TimetableViewModel.java
+++ 4-application/app/src/main/java/uk/ac/cardiff/c21048228/mycommute/ui/dashboard/TimetableViewModel.java
@@ -6,14 +6,5 @@ import androidx.lifecycle.ViewModel;
 
 public class TimetableViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
 
-    public TimetableViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
-    }
 }
