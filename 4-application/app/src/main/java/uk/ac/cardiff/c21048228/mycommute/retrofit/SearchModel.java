81a7aa61c7e6440bb8633341bfcd2c9e1b37dea8
notttj <tobynott80@gmail.com>
2023-04-02 18:09:27 +0100

    added getters and setters

--- 4-application/app/src/main/java/uk/ac/cardiff/c21048228/mycommute/retrofit/SearchModel.java
+++ 4-application/app/src/main/java/uk/ac/cardiff/c21048228/mycommute/retrofit/SearchModel.java
@@ -13,10 +13,83 @@ public class SearchModel {
         public String description;
         public String workingTime;
         public String publicTime;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCrs() {
+            return crs;
+        }
+
+        public void setCrs(String crs) {
+            this.crs = crs;
+        }
+
+        public String getTiploc() {
+            return tiploc;
+        }
+
+        public void setTiploc(String tiploc) {
+            this.tiploc = tiploc;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public String getSystem() {
+            return system;
+        }
+
+        public void setSystem(String system) {
+            this.system = system;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getWorkingTime() {
+            return workingTime;
+        }
+
+        public void setWorkingTime(String workingTime) {
+            this.workingTime = workingTime;
+        }
+
+        public String getPublicTime() {
+            return publicTime;
+        }
+
+        public void setPublicTime(String publicTime) {
+            this.publicTime = publicTime;
+        }
     }
 
     public class Filter{
         public Destination destination;
+
+        public Destination getDestination() {
+            return destination;
+        }
+
+        public void setDestination(Destination destination) {
+            this.destination = destination;
+        }
+
     }
 
     public class Location{
@@ -25,6 +98,46 @@ public class SearchModel {
         public String tiploc;
         public String country;
         public String system;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCrs() {
+            return crs;
+        }
+
+        public void setCrs(String crs) {
+            this.crs = crs;
+        }
+
+        public String getTiploc() {
+            return tiploc;
+        }
+
+        public void setTiploc(String tiploc) {
+            this.tiploc = tiploc;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public String getSystem() {
+            return system;
+        }
+
+        public void setSystem(String system) {
+            this.system = system;
+        }
     }
 
     public class LocationDetail{
@@ -46,6 +159,150 @@ public class SearchModel {
         public String cancelReasonShortText;
         public String cancelReasonLongText;
         public String displayAs;
+
+        public boolean isRealtimeActivated() {
+            return realtimeActivated;
+        }
+
+        public void setRealtimeActivated(boolean realtimeActivated) {
+            this.realtimeActivated = realtimeActivated;
+        }
+
+        public String getTiploc() {
+            return tiploc;
+        }
+
+        public void setTiploc(String tiploc) {
+            this.tiploc = tiploc;
+        }
+
+        public String getCrs() {
+            return crs;
+        }
+
+        public void setCrs(String crs) {
+            this.crs = crs;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getGbttBookedArrival() {
+            return gbttBookedArrival;
+        }
+
+        public void setGbttBookedArrival(String gbttBookedArrival) {
+            this.gbttBookedArrival = gbttBookedArrival;
+        }
+
+        public String getGbttBookedDeparture() {
+            return gbttBookedDeparture;
+        }
+
+        public void setGbttBookedDeparture(String gbttBookedDeparture) {
+            this.gbttBookedDeparture = gbttBookedDeparture;
+        }
+
+        public ArrayList<Origin> getOrigin() {
+            return origin;
+        }
+
+        public void setOrigin(ArrayList<Origin> origin) {
+            this.origin = origin;
+        }
+
+        public ArrayList<Destination> getDestination() {
+            return destination;
+        }
+
+        public void setDestination(ArrayList<Destination> destination) {
+            this.destination = destination;
+        }
+
+        public boolean isCall() {
+            return isCall;
+        }
+
+        public void setCall(boolean call) {
+            isCall = call;
+        }
+
+        public boolean isPublicCall() {
+            return isPublicCall;
+        }
+
+        public void setPublicCall(boolean publicCall) {
+            isPublicCall = publicCall;
+        }
+
+        public String getRealtimeArrival() {
+            return realtimeArrival;
+        }
+
+        public void setRealtimeArrival(String realtimeArrival) {
+            this.realtimeArrival = realtimeArrival;
+        }
+
+        public boolean isRealtimeArrivalActual() {
+            return realtimeArrivalActual;
+        }
+
+        public void setRealtimeArrivalActual(boolean realtimeArrivalActual) {
+            this.realtimeArrivalActual = realtimeArrivalActual;
+        }
+
+        public String getRealtimeDeparture() {
+            return realtimeDeparture;
+        }
+
+        public void setRealtimeDeparture(String realtimeDeparture) {
+            this.realtimeDeparture = realtimeDeparture;
+        }
+
+        public boolean isRealtimeDepartureActual() {
+            return realtimeDepartureActual;
+        }
+
+        public void setRealtimeDepartureActual(boolean realtimeDepartureActual) {
+            this.realtimeDepartureActual = realtimeDepartureActual;
+        }
+
+        public String getCancelReasonCode() {
+            return cancelReasonCode;
+        }
+
+        public void setCancelReasonCode(String cancelReasonCode) {
+            this.cancelReasonCode = cancelReasonCode;
+        }
+
+        public String getCancelReasonShortText() {
+            return cancelReasonShortText;
+        }
+
+        public void setCancelReasonShortText(String cancelReasonShortText) {
+            this.cancelReasonShortText = cancelReasonShortText;
+        }
+
+        public String getCancelReasonLongText() {
+            return cancelReasonLongText;
+        }
+
+        public void setCancelReasonLongText(String cancelReasonLongText) {
+            this.cancelReasonLongText = cancelReasonLongText;
+        }
+
+        public String getDisplayAs() {
+            return displayAs;
+        }
+
+        public void setDisplayAs(String displayAs) {
+            this.displayAs = displayAs;
+        }
     }
 
     public class Origin{
@@ -53,12 +310,68 @@ public class SearchModel {
         public String description;
         public String workingTime;
         public String publicTime;
+
+        public String getTiploc() {
+            return tiploc;
+        }
+
+        public void setTiploc(String tiploc) {
+            this.tiploc = tiploc;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public String getWorkingTime() {
+            return workingTime;
+        }
+
+        public void setWorkingTime(String workingTime) {
+            this.workingTime = workingTime;
+        }
+
+        public String getPublicTime() {
+            return publicTime;
+        }
+
+        public void setPublicTime(String publicTime) {
+            this.publicTime = publicTime;
+        }
     }
 
     public class Root{
         public Location location;
         public Filter filter;
         public ArrayList<Service> services;
+
+        public Location getLocation() {
+            return location;
+        }
+
+        public void setLocation(Location location) {
+            this.location = location;
+        }
+
+        public Filter getFilter() {
+            return filter;
+        }
+
+        public void setFilter(Filter filter) {
+            this.filter = filter;
+        }
+
+        public ArrayList<Service> getServices() {
+            return services;
+        }
+
+        public void setServices(ArrayList<Service> services) {
+            this.services = services;
+        }
     }
 
     public class Service{
@@ -73,6 +386,96 @@ public class SearchModel {
         public boolean isPassenger;
         public ArrayList<Origin> origin;
         public ArrayList<Destination> destination;
+
+        public LocationDetail getLocationDetail() {
+            return locationDetail;
+        }
+
+        public void setLocationDetail(LocationDetail locationDetail) {
+            this.locationDetail = locationDetail;
+        }
+
+        public String getServiceUid() {
+            return serviceUid;
+        }
+
+        public void setServiceUid(String serviceUid) {
+            this.serviceUid = serviceUid;
+        }
+
+        public String getRunDate() {
+            return runDate;
+        }
+
+        public void setRunDate(String runDate) {
+            this.runDate = runDate;
+        }
+
+        public String getTrainIdentity() {
+            return trainIdentity;
+        }
+
+        public void setTrainIdentity(String trainIdentity) {
+            this.trainIdentity = trainIdentity;
+        }
+
+        public String getRunningIdentity() {
+            return runningIdentity;
+        }
+
+        public void setRunningIdentity(String runningIdentity) {
+            this.runningIdentity = runningIdentity;
+        }
+
+        public String getAtocCode() {
+            return atocCode;
+        }
+
+        public void setAtocCode(String atocCode) {
+            this.atocCode = atocCode;
+        }
+
+        public String getAtocName() {
+            return atocName;
+        }
+
+        public void setAtocName(String atocName) {
+            this.atocName = atocName;
+        }
+
+        public String getServiceType() {
+            return serviceType;
+        }
+
+        public void setServiceType(String serviceType) {
+            this.serviceType = serviceType;
+        }
+
+        public boolean isPassenger() {
+            return isPassenger;
+        }
+
+        public void setPassenger(boolean passenger) {
+            isPassenger = passenger;
+        }
+
+        public ArrayList<Origin> getOrigin() {
+            return origin;
+        }
+
+        public void setOrigin(ArrayList<Origin> origin) {
+            this.origin = origin;
+        }
+
+        public ArrayList<Destination> getDestination() {
+            return destination;
+        }
+
+        public void setDestination(ArrayList<Destination> destination) {
+            this.destination = destination;
+        }
     }
+
+
 
 }
