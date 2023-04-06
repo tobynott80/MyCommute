7580c079f218241a15178cdd057d9cae65d4887f
notttj <tobynott80@gmail.com>
2023-04-06 18:18:27 +0100

    added recycler adapter for the train service recycler view

---
+++ 4-application/app/src/main/java/uk/ac/cardiff/c21048228/mycommute/ui/timetable/TrainServiceRecyclerAdapter.java
@@ -0,0 +1,70 @@
+package uk.ac.cardiff.c21048228.mycommute.ui.timetable;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import uk.ac.cardiff.c21048228.mycommute.R;
+
+public class TrainServiceRecyclerAdapter extends RecyclerView.Adapter<TrainServiceRecyclerAdapter.ViewHolder> {
+
+    private ArrayList<TrainService> trainServices;
+
+
+    @NonNull
+    @Override
+    public TrainServiceRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.train_service_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TrainServiceRecyclerAdapter.ViewHolder holder, int position) {
+        String platform = trainServices.get(position).getPlatform();
+        String departureTime = trainServices.get(position).getDepartureTime();
+        String origin = trainServices.get(position).getOrigin();
+        String destination = trainServices.get(position).getDestination();
+        TrainStatus status = trainServices.get(position).getStatus();
+        holder.platformNumber.setText(platform);
+        holder.departureTime.setText(departureTime);
+        holder.origin.setText(origin);
+        holder.destination.setText(destination);
+        if (status == TrainStatus.ON_TIME) {
+            holder.status.setText(R.string.on_time);
+        } else if (status == TrainStatus.DELAYED) {
+            holder.status.setText(R.string.delayed);
+        } else if (status == TrainStatus.CANCELLED) {
+            holder.status.setText(R.string.cancelled);
+        } else if (status == TrainStatus.NO_INFORMATION) {
+            holder.status.setText(R.string.no_information);
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return trainServices.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        private TextView platformNumber;
+        private TextView departureTime;
+        private TextView origin;
+        private TextView destination;
+        private TextView status;
+        public ViewHolder(final View view) {
+            super(view);
+            platformNumber = view.findViewById(R.id.tvPlatformNumber);
+            departureTime = view.findViewById(R.id.tvDeparture);
+            origin = view.findViewById(R.id.tvOrigin);
+            destination = view.findViewById(R.id.tvDestination);
+            status = view.findViewById(R.id.tvStatus);
+        }
+    }
+}
