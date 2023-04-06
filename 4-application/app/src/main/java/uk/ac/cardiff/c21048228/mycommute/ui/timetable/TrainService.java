ec4f844ccdb8ffaa2cc37968057a113c1350fd66
notttj <tobynott80@gmail.com>
2023-04-06 18:12:06 +0100

    added train service class for model

---
+++ 4-application/app/src/main/java/uk/ac/cardiff/c21048228/mycommute/ui/timetable/TrainService.java
@@ -0,0 +1,57 @@
+package uk.ac.cardiff.c21048228.mycommute.ui.timetable;
+
+public class TrainService {
+    private String platform;
+    private String departureTime;
+    private String origin;
+    private String destination;
+    private TrainStatus status;
+
+    public String getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(String departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public TrainStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TrainStatus status) {
+        this.status = status;
+    }
+
+    public TrainService(String platform, String departureTime, String origin, String destination, TrainStatus status) {
+        this.platform = platform;
+        this.departureTime = departureTime;
+        this.origin = origin;
+        this.destination = destination;
+        this.status = status;
+    }
+}
