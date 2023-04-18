9897e8dccbf90258efdbc366bc6d75da0adc73b2
notttj <tobynott80@gmail.com>
2023-04-18 17:09:10 +0100

    created commute fragment

---
+++ 4-application/app/src/main/java/uk/ac/cardiff/c21048228/mycommute/ui/home/CommuteFragment.java
@@ -0,0 +1,21 @@
+package uk.ac.cardiff.c21048228.mycommute.ui.home;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import uk.ac.cardiff.c21048228.mycommute.databinding.FragmentCommuteHomeBinding;
+
+public class CommuteFragment extends Fragment {
+    FragmentCommuteHomeBinding binding;
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+}
