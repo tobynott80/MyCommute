dd8120d4d4c70e7596dab91b0947a77c494c94b2
notttj <tobynott80@gmail.com>
2023-04-19 15:26:36 +0100

    fixed commute fragment (thanks GPT4!)

--- 4-application/app/src/main/java/uk/ac/cardiff/c21048228/mycommute/ui/home/CommuteFragment.java
+++ 4-application/app/src/main/java/uk/ac/cardiff/c21048228/mycommute/ui/home/CommuteFragment.java
@@ -1,7 +1,9 @@
 package uk.ac.cardiff.c21048228.mycommute.ui.home;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -17,14 +19,18 @@ public class CommuteFragment extends Fragment {
         this.commute = commute;
     }
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentCommuteHomeBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding = FragmentCommuteHomeBinding.bind(view);
-        binding.tvRouteDetails.setText("Test");
-        if(commute.isValid){
-            if (commute.services.size()>2){
+        if (commute.isValid) {
+            if (commute.services.size() > 2) {
                 binding.tvRouteDetails.setText(String.format("%s to %s", commute.Arrival, commute.Destination));
             }
         }
