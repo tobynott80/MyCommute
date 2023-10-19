957e76be21f9f61b3fed3a9a69f789762ca151ea
Toby Nott <tobynott80@gmail.com>
2023-10-19 19:15:19 +0100

    fixed silly refactoring mistake

--- app/app/src/main/java/uk/ac/cardiff/c21048229/mycommute/retrofit/CommuteCallback.java
+++
@@ -1,8 +0,0 @@
-package uk.ac.cardiff.c21048229.mycommute.retrofit;
-
-import uk.ac.cardiff.c21048229.mycommute.ui.home.Commute;
-
-public interface CommuteCallback {
-    void onCommuteLoaded(Commute commute);
-
-}
--- app/app/src/main/java/uk/ac/cardiff/c21048229/mycommute/ui/locationSelector/LocationSelectorViewModel.java
+++
@@ -1,6 +0,0 @@
-package uk.ac.cardiff.c21048229.mycommute.ui.locationSelector;
-
-import androidx.lifecycle.ViewModel;
-
-public class LocationSelectorViewModel extends ViewModel {
-}
